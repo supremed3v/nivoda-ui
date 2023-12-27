16215bb1510c70d37025d7197f710297b8211ba0
Saad Siddiqui <saadsiddiqui-programmer@protonmail.com>
2023-12-27 13:13:48 +0500

    completed order creation + checkout

--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -2,7 +2,7 @@ import { useCartContext } from "../../context/CartContext";
 import { useAuthContext } from "../../context/AuthContext";
 import previewImage from "../../assets/nopreview.jpg";
 import { loadStripe } from "@stripe/stripe-js";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import {
   CardElement,
   useStripe,
@@ -31,9 +31,32 @@ const CheckOutSection = ({ cartItems, getCartSubTotal, user }) => {
   const stripe = useStripe();
   const elements = useElements();
   const [loading, setLoading] = useState(false);
+  const [address, setAddress] = useState("");
+  const [email, setEmail] = useState("");
+  const [state, setState] = useState("");
+  const [zip, setZip] = useState(null);
+  const states = [
+    "Alabama",
+    "Alaska",
+    "American Samoa",
+    "Arizona",
+    "Arkansas",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "Delaware",
+  ];
 
   const handleSubmit = async () => {
     setLoading(true);
+
+    if (address === "" || email === "" || state === "" || zip === null) {
+      alert("Please fill all the fields");
+      setLoading(false);
+      return;
+    }
+    let clientAddress = `${address}, ${state}, ${zip.toString()}`;
+
     try {
       const amount = getCartSubTotal() / 100;
       const response = await axios.post(
@@ -55,6 +78,9 @@ const CheckOutSection = ({ cartItems, getCartSubTotal, user }) => {
       const result = await stripe.confirmCardPayment(client_secret, {
         payment_method: {
           card: elements.getElement(CardElement),
+        },
+        billing_details: {
+          email: email,
         },
       });
 
@@ -81,10 +107,9 @@ const CheckOutSection = ({ cartItems, getCartSubTotal, user }) => {
             order_date: new Date().toISOString(),
             order_status: "pending",
             stripe_paymentintent_id: paymentIntentId,
-            address: "test address",
+            address: clientAddress,
             order_items,
           };
-
           const response = await axios.post(
             "https://nivodabackend.beartales.net/index.php/wp-json/wp/v2/orders",
             data,
@@ -98,6 +123,7 @@ const CheckOutSection = ({ cartItems, getCartSubTotal, user }) => {
           if (response.data.status === "ORDER_CREATED") {
             console.log("Order Created");
             setLoading(false);
+            <Navigate to="/checkout/success" />;
           } else {
             console.log("Order Failed");
             console.log(response.data);
@@ -149,62 +175,6 @@ const CheckOutSection = ({ cartItems, getCartSubTotal, user }) => {
               </div>
             ))}
           </div>
-
-          <p className="mt-8 text-lg font-medium">Shipping Methods</p>
-          <form className="mt-5 grid gap-6">
-            <div className="relative">
-              <input
-                className="peer hidden"
-                id="radio_1"
-                type="radio"
-                name="radio"
-                checked
-              />
-              <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
-              <label
-                className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
-                htmlFor="radio_1"
-              >
-                <img
-                  className="w-14 object-contain"
-                  src="/images/naorrAeygcJzX0SyNI4Y0.png"
-                  alt=""
-                />
-                <div className="ml-5">
-                  <span className="mt-2 font-semibold">Fedex Delivery</span>
-                  <p className="text-slate-500 text-sm leading-6">
-                    Delivery: 2-4 Days
-                  </p>
-                </div>
-              </label>
-            </div>
-            <div className="relative">
-              <input
-                className="peer hidden"
-                id="radio_2"
-                type="radio"
-                name="radio"
-                checked
-              />
-              <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
-              <label
-                className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
-                htmlFor="radio_2"
-              >
-                <img
-                  className="w-14 object-contain"
-                  src="/images/oG8xsl3xsOkwkMsrLGKM4.png"
-                  alt=""
-                />
-                <div className="ml-5">
-                  <span className="mt-2 font-semibold">Fedex Delivery</span>
-                  <p className="text-slate-500 text-sm leading-6">
-                    Delivery: 2-4 Days
-                  </p>
-                </div>
-              </label>
-            </div>
-          </form>
         </div>
         <div className="mt-10 bg-gray-50 px-4 pt-8 lg:mt-0">
           <p className="text-xl font-medium">Payment Details</p>
@@ -225,6 +195,8 @@ const CheckOutSection = ({ cartItems, getCartSubTotal, user }) => {
                 name="email"
                 className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
                 placeholder="your.email@gmail.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
               <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
                 <svg
@@ -243,37 +215,7 @@ const CheckOutSection = ({ cartItems, getCartSubTotal, user }) => {
                 </svg>
               </div>
             </div>
-            <label
-              htmlFor="card-holder"
-              className="mt-4 mb-2 block text-sm font-medium"
-            >
-              Card Holder
-            </label>
-            <div className="relative">
-              <input
-                type="text"
-                id="card-holder"
-                name="card-holder"
-                className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
-                placeholder="Your full name here"
-              />
-              <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4 text-gray-400"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z"
-                  />
-                </svg>
-              </div>
-            </div>
+
             <label
               htmlFor="card-no"
               className="mt-4 mb-2 block text-sm font-medium"
@@ -298,6 +240,8 @@ const CheckOutSection = ({ cartItems, getCartSubTotal, user }) => {
                   name="billing-address"
                   className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
                   placeholder="Street Address"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
                 />
                 <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
                   <img
@@ -311,14 +255,23 @@ const CheckOutSection = ({ cartItems, getCartSubTotal, user }) => {
                 type="text"
                 name="billing-state"
                 className="w-full rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                value={state}
+                onChange={(e) => setState(e.target.value)}
               >
                 <option value="State">State</option>
+                {states.map((state) => (
+                  <option key={state} value={state}>
+                    {state}
+                  </option>
+                ))}
               </select>
               <input
-                type="text"
+                type="number"
                 name="billing-zip"
                 className="flex-shrink-0 rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none sm:w-1/6 focus:z-10 focus:border-blue-500 focus:ring-blue-500"
                 placeholder="ZIP"
+                value={zip}
+                onChange={(e) => setZip(e.target.value)}
               />
             </div>
 
